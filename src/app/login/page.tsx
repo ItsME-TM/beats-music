3af7600bf480b0833f6e292a9f918aa39ac574b7
ItsME-TM <thushanmalaka123@gmail.com>
2025-09-11 08:02:13 +0530

    Login form Basics input are complete

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,6 +6,9 @@ import Image from "next/image";
 
 export default function Login() {
     const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [rememberMe, setRememberMe] = useState(false);
+
     const handleLogin = () => {
         console.log('Login: ')
     }
@@ -29,17 +32,66 @@ export default function Login() {
                 </div>
                 
             </div>
-            <div className="w-[45%] flex flex-col items-center justify-center">
+            <div className="w-[40%] flex-col items-center justify-center pr-20 pt-10 pl-15">
                 {/* Right column content */}
-                <FormInput 
-                    type="email"
-                    placeholder="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    width="w-38"
-                    height="h-8"
-                />
-                <LoginButton text="Login" onClick={handleLogin} width="w-38" height="h-8"/>
+                <div
+                    className="relative w-90 rounded-xl p-8 backdrop-blur-[53px] shadow-[-8px_4px_5px_0px_#0000003D]"
+                    style={{
+                        background: `
+                        linear-gradient(0deg, rgba(0, 0, 0, 0.14), rgba(0, 0, 0, 0.14)),
+                        linear-gradient(321.23deg, rgba(191, 191, 191, 0.062) 5.98%, rgba(0, 0, 0, 0) 66.28%)
+                        `,
+                        border: "1px solid",
+                        borderImageSource: `
+                        linear-gradient(166.93deg, #AFAFAF 3.24%, rgba(96, 96, 96, 0) 96.43%),
+                        linear-gradient(317.92deg, rgba(255, 255, 255, 0.6) 1.48%, rgba(0, 0, 0, 0) 67.95%)
+                        `
+                    }}
+                >
+                    <h2 className="text-white text-2xl font-bold">Welcome Back</h2>
+                    <p className="text-gray-300 text-xs">Glad you’re back.!</p>
+                    <div className="flex flex-col  h-100 items-center">
+                        <div className="mt-3">
+                            <FormInput 
+                                type="email"
+                                placeholder="Username"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                width="w-72"
+                                height="h-10"
+                            />
+                        </div>
+                        <div className="mt-3">
+                            <FormInput 
+                                type="password"
+                                placeholder="Password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                width="w-72"
+                                height="h-10"
+                            />
+                        </div>
+                        <div className="flex items-center justify-start w-full mt-2 ml-3">
+                            <input
+                                type="checkbox"
+                                id="rememberMe"
+                                checked={rememberMe}
+                                onChange={() => setRememberMe(!rememberMe)}
+                                className="bg-gradient-to-b from-blue-500 via-blue-300 to-purple-500 w-4 h-4"
+                            />
+                            <label
+                                htmlFor="rememberMe"
+                                className="ml-2 text-white text-sm"
+                                style={{ fontFamily: 'Noto Sans, sans-serif' }}
+                            >
+                                Remember me
+                            </label>
+                        </div>
+                        <div>
+                            <LoginButton text="Login" onClick={handleLogin} width="w-72" height="h-10"/>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
